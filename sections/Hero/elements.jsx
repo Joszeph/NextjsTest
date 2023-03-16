8f97db3e264ee00dbc11ad41e6793cf91e21f6a3
Yosif Georgiev <iodesign@live.com>
2023-03-16 14:30:03 +0200

    UPDATE: Responsive Main section

--- sections/Hero/elements.jsx
+++ sections/Hero/elements.jsx
@@ -10,6 +10,9 @@ export const StyledContainer = styled(({ height, ...props }) => (
   <SectionContainer {...props} />
 ))`
   align-items: center;
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+  }
 `;
 
 export const StyledGetStartedBtn = styled((props) => (
@@ -36,10 +39,19 @@ export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
   width: 30%;
   margin-left: 11.125rem;
   font-family: sans-serif;
+  @media screen and (max-width: 1024px) {
+   margin:30px 0;
+   width: 50%;
+   text-align:center;
+  }
 `;
 
 export const StyledCTAContainer = styled(({ ...props }) => <div {...props} />)`
   display: flex;
+  @media screen and (max-width: 1024px) {
+    justify-content: center;
+    margin-top: -45px;
+   }
 `;
 
 export const StyledImageContainer = styled(({ ...props }) => (
@@ -50,4 +62,9 @@ export const StyledImageContainer = styled(({ ...props }) => (
   width: 100%;
   height: 100%;
   margin-left: auto;
+  @media screen and (max-width: 1024px) {
+    margin:30px 0 70px 0;
+    max-width: 26.03rem;
+    max-height: 65rem;
+   }
 `;
